addc852d601da1178d4ddcfecec874db7e9d7aad
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-02 12:37:07 +0300

    adds java

---
+++ index.js
@@ -0,0 +1,6 @@
+//const header = document.getElementById("header")
+header.style.textAlign = "center"
+const navbar = document.querySelectorAll("#navbar a")
+for(element of navbar){
+element.style.padding = "10px"
+}
