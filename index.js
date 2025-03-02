4a9547defaa5ee2bd62ba6044cac2ae2322898e5
Gabriel <kipkoechgabriel27@gmail.com>
2025-03-02 14:07:21 +0300

    adds css and java

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-//const header = document.getElementById("header")
+const header = document.getElementById("header")
 header.style.textAlign = "center"
+header.style.color ="white"
 const navbar = document.querySelectorAll("#navbar a")
 for(element of navbar){
 element.style.padding = "10px"
+element.style.textDecoration = "none"
+element.style.cursor = "pointer"
+element.style.color = "red"
 }
+
+const hero = document.getElementById("hero")
